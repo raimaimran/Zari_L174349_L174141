a8141230b1e64dafea40391b13329c10b7e3c0ca
raimaimran <raima.imran.ri@gmail.com>
2019-10-30 20:35:11 +0500

    buyer sign in

---
+++ app/src/main/java/com/example/project_zari/signinBuyer.java
@@ -0,0 +1,15 @@
+package com.example.project_zari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signinBuyer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin_buyer);
+
+    }
+}
