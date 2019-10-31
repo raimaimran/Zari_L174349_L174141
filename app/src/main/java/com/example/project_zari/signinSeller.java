8f4d2375a2075fdf7c06373fa346210211ae2aab
SanaBasharat <sana_basharat98@hotmail.com>
2019-10-31 21:01:16 +0500

    bottom nav made

--- app/src/main/java/com/example/project_zari/signinSeller.java
+++ app/src/main/java/com/example/project_zari/signinSeller.java
@@ -3,6 +3,7 @@ package com.example.project_zari;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +17,7 @@ public class signinSeller extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin_seller);
 
-        txt= (TextView) findViewById(R.id.sredirectsignup);
+        txt = (TextView) findViewById(R.id.sredirectsignup);
         txt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,7 +25,22 @@ public class signinSeller extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button button = (Button) findViewById(R.id.sssignin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //openSellerActivity();
+                Intent intent = new Intent(signinSeller.this, Seller_Home.class);
+                startActivity(intent);
+            }
+        });
     }
+
+//    public void openSellerActivity() {
+//        Intent intent = new Intent(signinSeller.this, Seller_Home.class);
+//        startActivity(intent);
+//    }
 
 }
 
